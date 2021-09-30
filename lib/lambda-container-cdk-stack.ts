d7c95a704de4a24429d9a22572f9db7adcf10855
adachishohei <dankan.0101@gmail.com>
2021-10-01 00:41:01 +0900

    諸々の追加

--- lib/lambda-container-cdk-stack.ts
+++ lib/lambda-container-cdk-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as path from "path";
+import * as iam from '@aws-cdk/aws-iam';
+
 
 export class LambdaContainerCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sampleFunction = new lambda.DockerImageFunction(this, 'sampleFunction', {
+      functionName: 'cdkSampleFunction',
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../src'), {
+        cmd: ['index.handler']
+      })
+    })
+
+    const cloudWatchLogsPolicyPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+      ]
+    });
+
+    cloudWatchLogsPolicyPolicy.addAllResources();
+
+    sampleFunction.addToRolePolicy(cloudWatchLogsPolicyPolicy);
   }
+
 }
